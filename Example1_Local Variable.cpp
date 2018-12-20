0a74f669dd7238f0a2edb74d9c3b6730279b05d1
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 13:54:12 +0800

    Update Example1_Local Variable.cpp

--- Example1_Local Variable.cpp
+++ Example1_Local Variable.cpp
@@ -1,8 +1,27 @@
+/**
+* @file Example1_Local Variable.cpp
+*
+* @brief C++ Program to demonstrate Local Variables
+*
+* @author Saif Ullah Ijaz
+*
+*/
+
 #include <iostream> 
 using namespace std;
 
+// FUNCTION PROTOTYPE (DECLARATION)
+
+/** function that tests local variables.
+*
+* @param void.
+*
+* @return void.
+*/
 void test();
 
+
+// function main begins program execution
 int main() {
 	int var = 5; // local variable to main() 
 	test();
@@ -11,9 +30,14 @@ int main() {
 	system("pause");
 	return 0;
 }
+// end main 
 
+// FUNCTION DEFINITION
+
+// function that tests local variables.
 void test() {
 	int var1; // local variable to test() 
 	var1 = 6;
 	cout << var; // illegal: var not visible inside test() 
 }
+// end function test

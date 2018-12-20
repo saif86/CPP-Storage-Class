f46d8b7e3cc1b0c731737997f024b36d335c5794
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2018-12-20 14:12:12 +0800

    Update Example3_Static Local Variable.cpp

--- Example3_Static Local Variable.cpp
+++ Example3_Static Local Variable.cpp
@@ -1,11 +1,31 @@
+/**
+* @file Example3_Static Local_Variable.cpp
+*
+* @brief C++ Program to demonstrate Static Local Variables. 
+*
+* @author Saif Ullah Ijaz
+*
+*/
+
 #include <iostream> 
 using namespace std;
 
+// FUNCTION DEFINITION
+
+/** function that tests static local variables.
+*
+* @param void.
+*
+* @return void.
+*/
 void test() {
 	static int var = 0; // var is a static variable; 
 	++var;
 	cout << var << endl;
 }
+
+
+// function main begins program execution
 int main() {
 	test();
 	test();
@@ -13,3 +33,4 @@ int main() {
 	system("pause");
 	return 0;
 }
+// end main 
